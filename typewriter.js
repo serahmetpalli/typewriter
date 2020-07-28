6bf1f157dcd3b33fd2918f4a139763c9e5ccf5eb
Serah Sirohi <serahsirohi@gmail.com>
2020-07-28 13:03:17 -0700

    adding typewriter file

---
+++ typewriter.js
@@ -0,0 +1,18 @@
+const sentence = "hello there from lighthouse labs";
+let delay = 0;
+
+for (const char of sentence) {
+  setTimeout(() => {
+    // console.log(char);
+    process.stdout.write(char);
+    ;
+  }, delay)
+  delay += 50;
+}
+
+ setTimeout(() => {
+  process.stdout.write("\n");
+}, delay)
+
+
+
